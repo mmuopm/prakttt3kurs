e3fb14eeea07af5f48f186e91861d107c8e576ef
mmuopm <mulyish@gmail.com>
2024-10-18 23:17:26 +0300

    gipgip

--- js/dark.js
+++ js/dark.js
@@ -23,3 +23,30 @@ const toggleButton = document.getElementById("theme-toggle");
 if (toggleButton) {
   toggleButton.addEventListener("click", toggleTheme);
 }
+
+
+/* темная тема */
+function toggleTheme() {
+  const htmlElement = document.documentElement;
+  const isDarkTheme = htmlElement.classList.toggle("dark_style");
+  localStorage.setItem("theme", isDarkTheme ? "dark" : "light");
+}
+
+// для применения сохр темы
+function applyTheme() {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.documentElement.classList.add("dark_style");
+  } else {
+    document.documentElement.classList.remove("dark_style");
+  }
+}
+
+//применить при загрузке страницы
+document.addEventListener("DOMContentLoaded", applyTheme);
+
+// обработчик события для кнопки переключения темы (только на главной)
+const toggleButton2 = document.getElementById("theme-toggle2");
+if (toggleButton2) {
+  toggleButton2.addEventListener("click", toggleTheme);
+}
--- js/main.js
+++ js/main.js
@@ -1,7 +1,7 @@
 /* бургер-меню */
 
 const burgerMenu = document.querySelector(".burger-menu");
-const subMenu = document.querySelector(".sub-menu-414");
+const subMenu = document.querySelector(".sub-menu-375");
 
 // Обработчик клика для открытия и закрытия подменю
 burgerMenu.addEventListener("click", () => {
@@ -9,7 +9,7 @@ burgerMenu.addEventListener("click", () => {
 });
 
 // Обработка ссылок внутри подменю
-const submenuLinks = document.querySelectorAll(".sub-menu-414-links a");
+const submenuLinks = document.querySelectorAll(".sub-menu-375-links a");
 
 submenuLinks.forEach((link) => {
   link.addEventListener(
@@ -23,53 +23,62 @@ submenuLinks.forEach((link) => {
   ); // 300ms (время анимации)
 });
 
-
-
 /* авторизация и регистрация */
+document.addEventListener("DOMContentLoaded", () => {
+  const menu = document.querySelector(".popup");
+  const openmenu = document.querySelector(".vhodd");
 
-const menu = document.querySelector(".popup");
-const openmenu = document.querySelector(".vhodd");
-const closemenu = document.querySelector(".popup .x");
+  const closemenu = document.querySelector(".popup .x");
 
-const menu2 = document.querySelector(".popup2");
-const openmenu2 = document.querySelector(".open-registr");
-const closemenu2 = document.querySelector(".popup2 .x");
+  const menu2 = document.querySelector(".popup2");
+  const openmenu2 = document.querySelector(".open-registr");
 
-const popups = document.querySelector(".popups");
+  const closemenu2 = document.querySelector(".popup2 .x");
 
-// Открытие первого меню
-openmenu.addEventListener("click", (e) => {
-  e.preventDefault();
-  popups.style.display = "flex"; // Показываем контейнер с попапами
-  menu.classList.add("activ");
+  const popups = document.querySelector(".popups");
+
+  // Открытие первого меню
+  openmenu.addEventListener("click", (e) => {
+    e.preventDefault();
+    popups.style.display = "flex"; // Показываем контейнер с попапами
+    menu.classList.add("activ");
+  });
+
+  // Закрытие первого меню
+  closemenu.addEventListener("click", () => {
+    menu.classList.remove("activ");
+    if (!menu2.classList.contains("activ")) {
+      popups.style.display = "none"; // Скрываем контейнер с попапами, если ни одно из меню не активно
+    }
+  });
+
+  // Открытие второго меню (регистрация)
+  openmenu2.addEventListener("click", (e) => {
+    e.preventDefault();
+    menu.classList.remove("activ");
+    popups.style.display = "flex"; // Показываем контейнер с попапами
+    menu2.classList.add("activ");
+  });
+
+  // Закрытие второго меню
+  closemenu2.addEventListener("click", () => {
+    menu2.classList.remove("activ");
+    if (!menu.classList.contains("activ")) {
+      popups.style.display = "none"; // Скрываем контейнер с попапами, если ни одно из меню не активно
+    }
+  });
+
+  // Обработчик для кнопки "Авторизоваться"
+  document.querySelector(".open-request").addEventListener("click", () => {
+    window.location.href = "../pages/request.html"; // Переход на страницу request
+  });
+
+  const openmenuMini = document.getElementById("vhodik");
+
+  openmenuMini.onclick = function () {
+    // l.preventDefault();
+    popups.style.display = "flex"; // Показываем контейнер с попапами
+    menu.classList.add("activ");
+    console.log(12);
+  };
 });
-
-// Закрытие первого меню
-closemenu.addEventListener("click", () => {
-  menu.classList.remove("activ");
-  if (!menu2.classList.contains("activ")) {
-    popups.style.display = "none"; // Скрываем контейнер с попапами, если ни одно из меню не активно
-  }
-});
-
-// Открытие второго меню (регистрация)
-openmenu2.addEventListener("click", (e) => {
-  e.preventDefault();
-  menu.classList.remove("activ");
-  popups.style.display = "flex"; // Показываем контейнер с попапами
-  menu2.classList.add("activ");
-});
-
-// Закрытие второго меню
-closemenu2.addEventListener("click", () => {
-  menu2.classList.remove("activ");
-  if (!menu.classList.contains("activ")) {
-    popups.style.display = "none"; // Скрываем контейнер с попапами, если ни одно из меню не активно
-  }
-});
-
-// Обработчик для кнопки "Авторизоваться"
-document.querySelector(".open-request").addEventListener("click", () => {
-  window.location.href = "../pages/request.html"; // Переход на страницу request
-});
-
